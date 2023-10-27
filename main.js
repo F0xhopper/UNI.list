9daa7ac16b6ff6e42b4881846b702fde7890d638
F0xhopper <135560075+F0xhopper@users.noreply.github.com>
2023-10-28 00:24:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from "/axios";
 
 const allPlaylist = document.querySelectorAll(".PlaylistContainer");
 const firstsongs = document.querySelector(".songsContainer");
